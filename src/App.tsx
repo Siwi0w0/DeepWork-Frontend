d3de3055c8c3a803f2d70129e2f29612d2f5e51b
kiwylai <kiwylai@gmail.com>
2025-01-09 22:47:10 +0100

    add calendar and timeAxis in the same page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import "./App.css";
 
-import Calendar from "./components/Calendar";
+import Calendar from "./components/Calendar/Calendar";
+import TimeAxis from "./components/timeAxis/TimeAxis";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>My Calendar</h1>
-      <Calendar />
+    <div className="container">
+      <div className="calendar-panel">
+        <Calendar />
+      </div>
+      <div className="time-axis-panel">
+        <TimeAxis />
+      </div>
     </div>
   );
 };
