9b2bee4b59c21b61d45bf7768a2ef94054ee3d46
kiwylai <kiwylai@gmail.com>
2025-01-20 21:27:44 +0100

    modify homepage style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import Calendar from "../components/calendar/Calendar";
 import TimeAxis from "../components/timeAxis/TimeAxis";
 import Timer from "../components/timer/Timer";
 import { useNavigate } from "react-router-dom";
+import "../css/Home.css";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
@@ -12,17 +13,21 @@ const Home: React.FC = () => {
     localStorage.removeItem("isLoggedIn");
     navigate("/");
   };
+
   return (
-    <div className="container">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="calendar-panel flex min-h-screen flex-col items-center justify-between p-24">
-        <Calendar />
-        <Timer />
-      </div>
-      <div className="time-axis-panel">
-        <TimeAxis />
+    <div className="home-container">
+      <Navbar />
+      <div className="content-container">
+        {/* left: calendar & timer */}
+        <div className="calendar-panel">
+          <Calendar />
+          <Timer />
+        </div>
+
+        {/* right: time-axis */}
+        <div className="time-axis-panel">
+          <TimeAxis />
+        </div>
       </div>
     </div>
   );
