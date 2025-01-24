0808cd6f4fa08a811ed43e0d6f9255a2c20b9de2
kiwylai <kiwylai@gmail.com>
2025-01-24 23:38:22 +0100

    change style format

--- src/components/banner/BannerNavbar.tsx
+++ src/components/banner/BannerNavbar.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./BannerNavbar.module.css";
+import styles from "./BannerNavbar.module.css";
 
 const BannerNavbar: React.FC = () => {
   return (
-    <nav className="navbar ">
-      <div className="logo">
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
         <Link to="/">Deep Work</Link>
       </div>
-      <div className="nav-links">
-        <Link to="/register" className="btn btn-signup">
+      <div className={styles["nav-links"]}>
+        <Link
+          to="/register"
+          className={`${styles.btn} ${styles["btn-signup"]}`}
+        >
           Sign Up
         </Link>
-        <Link to="/login" className="btn btn-signin">
+        <Link to="/login" className={`${styles.btn} ${styles["btn-signin"]}`}>
           Sign In
         </Link>
       </div>
