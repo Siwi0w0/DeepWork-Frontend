ac0018b90fd8f131f1fb6df53e956fd6671f5c43
Siwi0w0 <yufei.sun64@gmail.com>
2025-01-30 19:25:25 +0800

    update TimeAxis

--- src/components/timeAxis/TimeAxis.tsx
+++ src/components/timeAxis/TimeAxis.tsx
@@ -1,32 +1,42 @@
 import React from "react";
-import "./TimeAxis.css";
+import "./timeAxis.css";
 
 const TimeAxis: React.FC = () => {
   const hours = Array.from({ length: 24 }).map((_, hour) => hour);
 
   return (
-    <div className="h-full w-full bg-gray-100 py-4time-block-container ">
-      {hours.map((hour) => (
-        <div className="time-zone" key={hour}>
-          <div className="time-number">
-            <div>
-              <span
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  borderBottom: "1px solid #ddd",
-                  height: "40px",
-                }}
-              >
-                {hour.toString().padStart(2, "0")}:00
-              </span>
+    <div>
+      <div className="Date-title flex">
+        <h1 className="font-mono font-semibold text-4xl">Today</h1>
+        <div>02:17</div>
+      </div>
+      <div  className="time-block-container">
+        <div className="z-0 h-full w-full py-4">
+        {hours.map((hour) => (
+          <div className="time-zone" key={hour}>
+            <div className="time-number">
+              <div>
+                <span
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    height: "40px",
+                  }}
+                >
+                  {hour.toString().padStart(2, "0")}:00
+                </span>
+              </div>
+            </div>
+            {/* Event 入口？ */}
+            <div className="time-axis">
+              <div style={{ flexGrow: 1,
+                backgroundColor: "#f9f9f9" }}></div>
             </div>
           </div>
-          <div className="time-axis">
-            <div style={{ flexGrow: 1, backgroundColor: "#f9f9f9" }}></div>
-          </div>
+        ))}
         </div>
-      ))}
+        <div className="z-1">event</div>
+      </div>
     </div>
   );
 };
